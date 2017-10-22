eb396420b3775f8de8f142c17a65f05920160f12
elyons <lyonsden@gmail.com>
2017-10-21 20:47:51 -0700

    added sparkle effects

--- src/AddressableStrip.h
+++ src/AddressableStrip.h
@@ -118,6 +118,9 @@ class AddressableStrip
     void bullet(String color, float span, int time, int dir);
 	  void RGBMatrix(int rows, int r, int g, int b, int span, int dir);
     void Matrix2RGB(float r1, float g1, float b1, float r2, float g2, float b2, float span, int time, int dir);
+    void Matrix2Color(String color1, String color2, float span, int time, int dir);
+		void sparkle (String color, int speed);
+		void sparkleRGB (int r, int g, int b, int speed);
 
   private:
     int _pin; //data pin 
