47b25829b8e46fdd48f8f82f2a0a1ba732cd3d84
elyons <lyonsden@gmail.com>
2017-10-22 19:37:35 -0700

    added in several function for creating multiple bullets

--- src/AddressableStrip.h
+++ src/AddressableStrip.h
@@ -111,10 +111,15 @@ class AddressableStrip
 		void RGBBandCont(int pos, int r, int g, int b, int span, int startLED, int endLED);
 	
 	  void RGBBullet(int pos, int r, int g, int b, int span, int dir);
+	  void RGBBulletCont(int pos, int r, int g, int b, int span, int dir);
 	  void bullet2RGB(float r1, float g1, float b1, float r2, float g2, float b2, float span, int time, int dir);
     void bullet2Color(String color1, String color2, float span, int time, int dir);
     void bulletRGB(int r, int g, int b, int span, int time, int dir);
     void bullet(String color, float span, int time, int dir);
+	  void multiBullet2RGB(float r1, float g1, float b1, float r2, float g2, float b2, float span, int time, int dir, int num);
+    void multiBullet2Color(String color1, String color2, float span, int time, int dir, int num);
+    void multiBulletRGB(int r, int g, int b, int span, int time, int dir, int num);
+    void multiBullet(String color, float span, int time, int dir, int num);
 	  void RGBMatrix(int rows, int r, int g, int b, int span, int dir);
     void Matrix2RGB(float r1, float g1, float b1, float r2, float g2, float b2, float span, int time, int dir);
     void Matrix2Color(String color1, String color2, float span, int time, int dir);
