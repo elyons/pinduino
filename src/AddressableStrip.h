857714060bcb754c1f7936930921305f73d0c267
elyons <lyonsden@gmail.com>
2019-10-15 08:16:29 -0700

    added two new functions for creating bullet effects that start and stop at specific LEDs

--- src/AddressableStrip.h
+++ src/AddressableStrip.h
@@ -124,6 +124,8 @@ class AddressableStrip
     void multiBullet2Color(String color1, String color2, float span, int time, int dir, int num);
     void multiBulletRGB(int r, int g, int b, int span, int time, int dir, int num);
     void multiBullet(String color, float span, int time, int dir, int num);
+    void bulletP2P_2RGB(float r1, float g1, float b1, float r2, float g2, float b2, float span, int time, int dir, int start, int stop);
+    void bulletP2P_2Color(String color1, String color2, float span, int time, int dir, int start, int stop);
     void RGBMatrix(int rows, int r, int g, int b, int span, int dir);
     void Matrix2RGB(float r1, float g1, float b1, float r2, float g2, float b2, float span, int time, int dir);
     void Matrix2Color(String color1, String color2, float span, int time, int dir);
